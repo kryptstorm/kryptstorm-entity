9872eb953d101d14b32c367c3b73ac7a2baa486f
Kryptstorm <kryptstorm@gmail.com>
2017-08-20 19:56:26 +0700

    Add asyncNative$

---
+++ src/index.js
@@ -0,0 +1,207 @@
+// External modules
+import Bluebird from "bluebird";
+import _ from "lodash";
+
+// Default options of this plugin
+const defaultOptions = {
+  limit$: 20, // Default limit of entity result (use for asyncList$)
+  fields$: ["id"] // Default return fields
+};
+
+export default function XEntity(options) {
+  // Merge the options of user with plugin options
+  // User's options have more important than plugin options
+  const _limit =
+    _.isNumber(options.limit$) && options.limit$ > 0
+      ? options.limit$
+      : defaultOptions.limit$;
+  const _fields =
+    _.isArray(options.fields$) && !_.isEmpty(options.fields$)
+      ? options.fields$
+      : defaultOptions.fields$;
+
+  // Retrive Seneca Enitty
+  const Entity = this.private$.exports.Entity.prototype;
+
+  /**
+	 * Assume you have an entity, let enity = this.make$("dog");
+	 * enity.fields$ is a FUNCTION return what should be saved to entity
+	 * 
+	 * @param {object} opts 
+	 * - opts.fields$ <Array> how many fields should be return
+	 * @param {bool} returnEntity Return enitty instance instead of entity object
+	 */
+  Entity.asyncSave$ = function asyncSave$(opts = {}, returnEntity = false) {
+    // Resolve options
+    if (!_.isArray(opts.fields$) || _.isEmpty(opts.fields$)) opts.fields$ = [];
+
+    // Create async method for seneca.entity.save$
+    const _asyncSave$ = Bluebird.promisify(Entity.save$, {
+      context: this
+    });
+
+    // Return entity instance instead of entity object
+    if (returnEntity) {
+      return _asyncSave$();
+    }
+
+    // Save and return entity object
+    return _asyncSave$().then(ent =>
+      Bluebird.resolve(_formatEntity(ent, [..._fields, ...opts.fields$]))
+    );
+  };
+
+  /**
+	 * Load one entity
+	 * 
+	 * @param {object} query 
+	 * - query.fields$ <Array> how many fields should be return
+	 * - query.native$ <Array|Object> (array -  use first element as query, second element as meta settings, object - use object as query, no meta settings)
+	 * @param {bool} returnEntity Return enitty instance instead of entity object
+	 */
+  Entity.asyncLoad$ = function asyncLoad$(query = {}, returnEntity = false) {
+    // Resolve query
+    if (!_.isObject(query)) query = {};
+    if (!_.isArray(query.fields$) || _.isEmpty(query.fields$)) {
+      query.fields$ = _fields;
+    }
+
+    // Create async method for seneca.entity.load$
+    const _asyncLoad$ = Bluebird.promisify(Entity.load$, {
+      context: this
+    });
+
+    // Return entity instance instead of entity object
+    if (returnEntity) return _asyncLoad$(query);
+
+    // Return entity object
+    return _asyncLoad$(query).then(ent => Bluebird.resolve(_formatEntity(ent)));
+  };
+
+  /**
+	 * Load list entity
+	 * 
+	 * @param {object} query 
+	 * - query.fields$ <Array> how many fields should be return
+	 * - query.limit$ number of entities should be return
+	 * - query.skip$ number of entities should be skip
+	 * - query.sort$ {field_1: -1, field_2: 1}
+	 * - query.native$ <Array|Object> (array -  use first element as query, second element as meta settings, object - use object as query, no meta settings)
+	 * @param {bool} returnEntity Return enitty instance instead of entity object
+	 */
+  Entity.asyncList$ = function asyncList$(query = {}, returnEntity = false) {
+    // Resolve query
+    if (!_.isObject(query)) query = {};
+    if (!_.isArray(query.fields$) || _.isEmpty(query.fields$)) {
+      query.fields$ = _fields;
+    }
+
+    // Create async method for seneca.entity.load$
+    const _asyncList$ = Bluebird.promisify(Entity.list$, {
+      context: this
+    });
+
+    // Return entity instance instead of entity object
+    if (returnEntity) return _asyncList$(query);
+
+    // Return array of entity object
+    return _asyncList$(query).then(ents => {
+      let result = [];
+      _.each(ents, ent => result.push(_formatEntity(ent)));
+      return Bluebird.resolve(result);
+    });
+  };
+
+  /**
+	 * Remove entities
+	 * 
+	 * @param {object} query 
+	 * - query.fields$ <Array> how many fields should be return
+	 * - query.all$ (default is false) Delete all entities match condition
+	 * - query.load$ (default is true) Return data after deleted an entity
+	 * - query.native$ <Array|Object> (array -  use first element as query, second element as meta settings, object - use object as query, no meta settings)
+	 */
+  Entity.asyncRemove$ = function asyncRemove$(query = {}) {
+    // Resolve query
+    if (!_.isBoolean(query.all$) || !query.all$) {
+      query.all$ = false;
+    }
+    if (!_.isBoolean(query.load$) || !query.load$) {
+      query.load$ = true;
+    }
+    if (!_.isArray(query.fields$) || _.isEmpty(query.fields$)) {
+      query.fields$ = [];
+    }
+
+    const _asyncRemove$ = Bluebird.promisify(Entity.remove$, {
+      context: this
+    });
+
+    return _asyncRemove$(query).then(attributes => {
+      // Delete many fields, we cannot return deleted data because seneca return empty data,
+      // just return empty array
+      if (query.all$) return Bluebird.resolve([]);
+
+      // Delete 1 row and load data after deleted
+      if (query.load$) {
+        return Bluebird.resolve(
+          _formatEntity(attributes, [..._fields, ...query.fields$])
+        );
+      }
+
+      // Delete 1 row and keep slient
+      return Bluebird.resolve({});
+    });
+  };
+
+  /**
+   * Connect node-mongodb-native
+   * @return connection to node-mongodb-native
+   */
+  Entity.asyncNative$ = function asyncNative$() {
+    const _asyncNative$ = Bluebird.promisify(entityClass.native$, {
+      context: this
+    });
+
+    return _asyncNative$();
+  };
+
+  // Register our method
+  const XEntity$ = {};
+  // Inject our method to seneca
+  this.decorate("XEntity$", XEntity$);
+
+  this.add("init:XEntity", function initXEntity(args, done) {
+    return done();
+  });
+
+  return { name: "XEntity" };
+}
+
+const _formatEntity = (ent, fields$) => {
+  let result = {},
+    attributes;
+
+  // Invalid entity
+  if (!_.isObject(ent) || _.isEmpty(ent)) return result;
+
+  // ent is seneca entity - ent.data$ is a function
+  if (_.isFunction(ent.data$)) {
+    attributes = ent.data$();
+  } else {
+    // Result of remove function is a mongo object, convert it to entity-like
+    attributes = _.merge(ent, { id: ent._id.toString() });
+  }
+
+  // Only return fields has been defined on fields$ if fields$ is defined
+  const _attributes = _.isArray(fields$)
+    ? _.pick(attributes, fields$)
+    : attributes;
+  // Only get public propeties
+  _.each(
+    _attributes,
+    (v, c) => (!_.includes(c, "$") ? (result[c] = v) : result)
+  );
+
+  return result;
+};
---
+++ src/test.js
@@ -0,0 +1,158 @@
+// External modules
+import Seneca from "seneca";
+import Bluebird from "bluebird";
+import { expect } from "chai";
+
+// Internal modules
+import XEntity from ".";
+
+// Defined function - what will return test app instance
+const App = fn =>
+  Seneca({
+    log: "test",
+    debug: { undead: true }
+  }).test(fn);
+
+// Defined basic test
+describe("XEntity - Basic", function() {
+  let id,
+    returnFields = ["name", "createdAt", "status"];
+  // Init test app
+  const app = App();
+  // Use memory store
+  app.use("basic");
+  // Use seneca-entity
+  app.use("entity");
+
+  // Register XEntity
+  app.use(XEntity);
+
+  // Before hook for test, all thing you need will be prepare at there
+  before(done => {
+    // App is ready to test
+    app.ready(() => done());
+  });
+
+  it("Create", function(done) {
+    let dog = app.make$("dogs");
+    dog.name = "Mihoo";
+    dog.createdAt = new Date();
+    dog.status = 1; // LIVE
+    dog
+      .asyncSave$({ fields$: returnFields })
+      .then(savedDog => {
+        // By default, result of asyncSave$ will be convert to object
+        expect(savedDog).to.be.an("object");
+
+        // All properties has been saved
+        expect(savedDog.name).to.be.equal(dog.name);
+        expect(savedDog.createdAt.toString()).to.be.equal(
+          dog.createdAt.toString()
+        );
+        expect(savedDog.status).to.be.equal(dog.status);
+
+        // Id must be auto created
+        expect(savedDog.id).to.be.exist;
+
+        // Store id
+        id = savedDog.id;
+        // Test is successful
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Read one row", function(done) {
+    let dog = app.make$("dogs");
+    dog
+      .asyncLoad$({ id, fields$: returnFields })
+      .then(loadedDog => {
+        // By default, result of asyncLoad$ will be convert to object
+        expect(loadedDog).to.be.an("object");
+
+        // All properties has been loaded
+        expect(loadedDog.name).to.be.exist;
+        expect(loadedDog.createdAt).to.be.exist;
+        expect(loadedDog.status).to.be.exist;
+
+        // Test is successful
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Read many row", function(done) {
+    let dog = app.make$("dogs");
+    dog
+      .asyncList$({ fields$: returnFields })
+      .then(loadedDogs => {
+        // By default, result of asyncList$ will be convert to array of object
+        expect(loadedDogs).to.be.an("array");
+        expect(loadedDogs[0]).to.be.an("object");
+
+        // All properties has been loaded
+        expect(loadedDogs[0].name).to.be.exist;
+        expect(loadedDogs[0].createdAt).to.be.exist;
+        expect(loadedDogs[0].status).to.be.exist;
+
+        // Test is successful
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Update", function(done) {
+    let dog = app.make$("dogs"),
+      newName = "Gau";
+    dog
+      .asyncLoad$({ id, fields$: returnFields }, true) // The second params will make asyncLoad$ return entity instead of object
+      .then(loadedDog => {
+        // By default, result of asyncLoad$ will be convert to object
+        expect(loadedDog).to.be.an("object");
+
+        // All properties has been loaded
+        expect(loadedDog.name).to.be.exist;
+        expect(loadedDog.createdAt).to.be.exist;
+        expect(loadedDog.status).to.be.exist;
+
+        loadedDog.name = newName;
+        return loadedDog.asyncSave$({ fields$: returnFields });
+      })
+      .then(savedDog => {
+        // By default, result of asyncSave$ will be convert to object
+        expect(savedDog).to.be.an("object");
+
+        // All properties has been saved
+        expect(savedDog.name).to.be.equal(newName);
+
+        // Id must be keep
+        expect(savedDog.id).to.be.equal(id);
+
+        // Test is successful
+        done();
+      })
+      .catch(done);
+  });
+
+  it("Delete", function(done) {
+    let dog = app.make$("dogs");
+    dog
+      .asyncRemove$({ id, fields$: returnFields })
+      .then(removedDog => {
+        // By default, result of asyncRemove$ will be convert to object
+        expect(removedDog).to.be.an("object");
+
+        // All properties has been return after delete
+        expect(removedDog.name).to.be.exist;
+        expect(removedDog.createdAt).to.be.exist;
+        expect(removedDog.status).to.be.exist;
+
+        // Delete the right entity
+        expect(removedDog.id).to.be.equal(id);
+
+        // Test is successful
+        done();
+      })
+      .catch(done);
+  });
+});
